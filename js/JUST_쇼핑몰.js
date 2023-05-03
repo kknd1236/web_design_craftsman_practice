19ed23649ba33c93c503d885df3965ba1ded9b54
parkhaesun <hspark@makebot.ai>
2023-05-03 17:59:07 +0900

    슬라이드 까지 완성

--- js/JUST_쇼핑몰.js
+++ js/JUST_쇼핑몰.js
@@ -10,4 +10,16 @@ $(function () {
             $('header > nav .sub').stop().slideUp();
         }
     );
+
+    $('.slide_all li').eq(0).siblings().css('top', '-300px');
+
+    let index = 1;
+
+    setInterval(() => {
+        if (index > 1) index = 0;
+        $('.slide_all>li').eq(index).siblings().animate({ top: '-300px' }, 500);
+        $('.slide_all>li').eq(index).animate({ top: '0' }, 500);
+        index++;
+        console.log(index);
+    }, 3000);
 });
